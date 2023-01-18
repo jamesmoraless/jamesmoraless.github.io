af7c77588ff257e8123e087c4c0a452dc1e87e86
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2023-01-18 00:37:02 -0500

    updated contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,18 +16,18 @@ const Contact = () => {
 
                     <div className="contact_form-group">
                         <div className="contact_form-div">
-                            <input type="text" className="contact_form-input" placeholder='Insert your name' required/>
+                            <input type="text" className="contact_form-input" name="name" class="form-control" placeholder='Insert your name' required/>
                         </div>
 
                         <div className="contact_form-div">
-                            <input type="email" className="contact_form-input" placeholder='Insert your email'/>
+                            <input type="email" className="contact_form-input" class="form-control" name="email" placeholder='Insert your email'required/>
                         </div>
                     </div>
                     <div className="contact_form-div">
                             <input type="text" className="contact_form-input" placeholder='Insert your subject'/>
                         </div>
                         <div className="contact_form-div contact_form-area">
-                            <textarea name="" id="" cols="30" rows="10" className='contact_form-input' placeholder='Write your message here'></textarea>
+                            <textarea name="message" id="" cols="30" rows="10" class="form-control" className='contact_form-input' placeholder='Write your message here' required></textarea>
                         </div>
 
                         <button className='btn'>Send Message</button>
