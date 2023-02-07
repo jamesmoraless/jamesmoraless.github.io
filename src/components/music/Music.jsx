14fe0305bf62c345472d4116f648ee5002ee5c13
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2023-02-07 10:58:46 -0500

    rework playlist

--- src/components/music/Music.jsx
+++ src/components/music/Music.jsx
@@ -5,7 +5,7 @@ const Music = () => {
     return (
         <section className="more container section" id='music'>
             <h2 className="section_title">What I Listen To</h2>
-            <iframe src="https://open.spotify.com/embed/playlist/7efziCcJHXFXcNrG2ApGxm?utm_source=generator&theme=0" className='music_frame'></iframe>
+            <iframe src="https://open.spotify.com/embed/playlist/5n2XgKVes7YbqeGuQcMdl4?utm_source=generator&theme=0" className='music_frame'></iframe>
 
 
         </section>
